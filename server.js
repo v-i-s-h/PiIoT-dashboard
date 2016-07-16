545a0e8ac063731549ece20b21bebeaa251173d8
v-i-s-h <get_vichu@yahoo.com>
2016-07-16 19:35:13 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+var http  = require("http"),
+    url   = require("url"),
+    path  = require("path"),
+    fs    = require("fs"),
+    mime  = require("mime")
+    port  = process.argv[2] || 8080;
+
+http.createServer( function( request, response) {
+
+    var uri = url.parse( request.url ).pathname;
+    var filename = path.join( process.cwd()+"/www", uri );
+
+    console.log( "New Request: \n" +
+                 "    URI : " + uri + "\n" +
+                 "    file: " + filename + "\n" );
+
+    fs.exists( filename, function(exists) {
+        if( !exists ) {
+            response.writeHead( 404, {"Content-Type": "text/plain"} );
+            response.write( "404 Not Found\n" );
+            response.end();
+            return;
+        }
+        if ( fs.statSync(filename).isDirectory() ) {
+            filename += '/index.html';
+        }
+        fs.readFile( filename, "binary", function(err, file ) {
+            if(err) {
+                response.writeHead( 500, {"Content-Type": "text/plain"} );
+                response.write( err + "\n" );
+                response.end();
+                return;
+            }
+            response.writeHead( 200, {"Content-Type": mime.lookup(filename)} );
+            response.write( file, "binary" );
+            response.end();
+        });
+    });
+}).listen( parseInt(port,10) );
+
+console.log( "Dashboard Server running as PORT " + port + "\nPress CTRL + C to stop" );
